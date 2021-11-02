054c80dece3a39cc537044f573ee569031367723
Eugene Zhemchugov <jini.zh@gmail.com>
2021-11-02 16:09:18 +0300

    Implement examples/survival

---
+++ examples/survival/survival.cpp
@@ -0,0 +1,336 @@
+#include "proton.hpp"
+
+#include <iostream>
+#include <list>
+#include <mutex>
+#include <sstream>
+#include <thread>
+
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/sysinfo.h>
+
+#include <getopt.h>
+
+void usage(const char* argv0) {
+  std::cout
+    << "This program calculates photon-photon luminosities and the survival "
+       "factor in proton-proton collisions\n"
+       "Usage: " << argv0 << " options...\n"
+       "Allowed options:\n"
+       "  -h or --help:             print this message and exit (no argument)\n"
+       "  -E or --collision-energy: specify proton-proton collision energy (in GeV). Default is 13e3\n"
+       "  -f or --from:             beginning of the invariant mass range (GeV)\n"
+       "  -t or --to:               ending of the invariant mass range (GeV)\n"
+       "  -n or --npoints:          number of points in the invariant mass range\n"
+       "  -s or --step:             points step in the invariant mass range (GeV)\n"
+       "  -l or --log:              assume logarithmic scale in the invariant mass range (no argument)\n"
+    << "  -v or --verbose:          be verbose while calculating\n"
+       "  --survival:               output file for the survival factor\n"
+       "  --luminosity:             output file for the luminosity with non-electromagnetic interactions between the protons neglected\n"
+       "  --luminosity_b:           output file for the luminosity with non-electromagnetic interactions between the protons respected\n"
+       "  --parallel:               output file for the luminosity of photons with parallel polarization and non-electromagnetic interactions between the protons respected\n"
+       "  --perpendicular:          output file for the luminosity of photons with perpendicular polarization and non-electromagnetic interactions between the protons respected\n";
+};
+
+std::vector<std::pair<double, double>> make_grid(
+    double   from,
+    double   to,
+    unsigned n,
+    bool     log = false
+) {
+  double x = from;
+  double step = log ? pow(to / from, 1. / n) : (to - from) / n;
+  std::vector<std::pair<double, double>> grid(n);
+  --n;
+  for (size_t i = 0; i < n; ++i) {
+    grid[i].first = x;
+    x = log ? x * step : x + step;
+  };
+  grid[n].first = to;
+  return grid;
+};
+
+epa::Function1d make_function1d(
+    const std::function<double (double)>& f,
+    std::vector<std::pair<double, double>> grid
+) {
+  epa::Function1d F(std::move(grid));
+  for (size_t i = 0; i < F.points.size(); ++i)
+    F.points[i].second = f(F.points[i].first);
+  return F;
+};
+
+epa::Function1d make_function1d_async(
+    const std::function<std::function<double (double)> ()>& generator,
+    std::vector<std::pair<double, double>> grid,
+    const std::string& verbose = std::string()
+) {
+  epa::Function1d F(std::move(grid));
+  std::mutex m_queue;
+  std::mutex m_output;
+  std::list<std::thread> threads;
+  size_t i = 0;
+  int nprocs = get_nprocs();
+  if (!verbose.empty()) std::cout << "Using " << nprocs << " threads.\n";
+  for (int t = 0; t < nprocs; ++t)
+    threads.emplace_back(
+        [&]() {
+          auto f = generator();
+          while (true) {
+            size_t j;
+            {
+              std::unique_lock lock(m_queue);
+              j = i++;
+            };
+            if (j >= F.points.size()) break;
+            F.points[j].second = f(F.points[j].first);
+            if (!verbose.empty()) {
+              std::unique_lock lock(m_output);
+              std::cout
+                << verbose
+                << F.points[j].first << " => " << F.points[j].second
+                << '\n';
+            };
+          };
+        }
+    );
+
+  for (auto& t: threads) t.join();
+  return F;
+};
+
+epa::Function1d pp_luminosity_b(
+    double collision_energy,
+    epa::Polarization polarization,
+    std::vector<std::pair<double, double>> grid,
+    const std::string& verbose = std::string()
+) {
+  return make_function1d_async(
+      [&]() -> std::function<double (double)> {
+        return [=, l = epa::pp_luminosity_b(collision_energy)](
+            double rs
+        ) -> double {
+          return l(rs, polarization);
+        };
+      },
+      std::move(grid),
+      verbose
+  );
+};
+
+double parse_energy(const char* arg, const char* value) {
+  char* end;
+  errno = 0;
+  double result = strtod(optarg, &end);
+  if (!*end && !errno && result > 0) return result;
+  std::cerr << "Invalid " << value << ": " << arg;
+  if (errno) std::cerr << " (" << strerror(errno) << ')';
+  else if (result <= 0) std::cerr << " (not positive)";
+  std::cerr << '\n';
+  std::exit(1);
+};
+
+unsigned parse_unsigned(const char* arg, const char* value) {
+  size_t end;
+  int result = std::stoi(arg, &end);
+  if (!arg[end] && result > 0) return result;
+  std::cerr << "Invalid " << value << ": " << arg;
+  if (result < 0) std::cerr << " (not positive)";
+  return result;
+};
+
+int main(int argc, char** argv) {
+  const char* survival      = nullptr;
+  const char* luminosity    = nullptr;
+  const char* luminosity_b  = nullptr;
+  const char* parallel      = nullptr;
+  const char* perpendicular = nullptr;
+
+  option options[] = {
+    { "help",             0, nullptr, 'h' },
+    { "collision-energy", 1, nullptr, 'E' },
+    { "from",             1, nullptr, 'f' },
+    { "to",               1, nullptr, 't' },
+    { "npoints",          1, nullptr, 'n' },
+    { "step",             1, nullptr, 's' },
+    { "log",              0, nullptr, 'l' },
+    { "verbose",          0, nullptr, 'v' },
+    { "luminosity",       1, nullptr, -2 },
+    { "luminosity_b",     1, nullptr, -3 },
+    { "parallel",         1, nullptr, -4 },
+    { "perpendicular",    1, nullptr, -5 },
+    { "survival",         1, nullptr, -6 },
+    { 0,                  0, 0,        0 }
+  };
+
+  std::string optstring;
+  {
+    std::stringstream ss;
+    for (auto& o: options)
+      if (o.val > 0) {
+        ss << static_cast<char>(o.val);
+        if (o.has_arg) ss << ':';
+      };
+    optstring = ss.str();
+  };
+
+  double collision_energy = 13e3;
+  double from = -1;
+  double to = -1;
+  double step = 0;
+  bool log = false;
+  unsigned npoints = 0;
+  bool verbose = false;
+
+  while (true) {
+    int c = getopt_long(argc, argv, optstring.c_str(), options, nullptr);
+    if (c == -1) break;
+    switch (c) {
+      case 'h':
+        usage(argv[0]);
+        return 0;
+
+      case 'E':
+        collision_energy = parse_energy(optarg, "collision energy");
+        break;
+
+      case 'f':
+        from = parse_energy(optarg, "beginning invariant mass (from)");
+        break;
+
+      case 't':
+        to = parse_energy(optarg, "ending invariant mass (to)");
+        break;
+
+      case 's':
+        step = parse_energy(optarg, "invariant mass step");
+        break;
+
+      case 'n':
+        npoints = parse_unsigned(optarg, "number of points");
+        break;
+
+      case 'l':
+        log = true;
+        break;
+
+      case 'v':
+        verbose = true;
+        break;
+
+      case -2:
+        luminosity = optarg;
+        break;
+
+      case -3:
+        luminosity_b = optarg;
+        break;
+
+      case -4:
+        parallel = optarg;
+        break;
+
+      case -5:
+        perpendicular = optarg;
+        break;
+
+      case -6:
+        survival = optarg;
+        break;
+
+
+      case '?':
+        return 1;
+    };
+  };
+
+  if ((from > 0) + (to > 0) + (step > 0) + (npoints > 0) != 3) {
+    std::cerr << "Exactly 3 of --from, --to, --step, --npoints must be specified\n";
+    return 1;
+  };
+
+  if (from < 0) {
+    from = to - step * npoints;
+    if (from < 0) {
+      std::cerr << "Beginning of the invariant mass range cannot be negative or zero\n";
+      return 1;
+    };
+  };
+
+  if (!npoints) npoints = (to - from) / step + 1;
+
+  auto grid = make_grid(from, to, npoints, log);
+
+  if (verbose)
+    std::cout
+      << "Calculating photon-photon luminosities in ultraperipheral collisions "
+         "of protons with the energy " << collision_energy << " GeV for "
+         "invariant masses from " << from << " GeV to " << to << " GeV ("
+      << npoints << " points)\n";
+
+  epa::Function1d f_luminosity;
+  if (luminosity || survival) {
+    if (verbose)
+      std::cout
+        << "  with non-electromagnetic interactions between protons "
+           "neglected... ";
+    f_luminosity = make_function1d(epa::pp_luminosity(collision_energy), grid);
+    if (verbose) std::cout << "done.";
+    if (luminosity) {
+      f_luminosity.save(luminosity);
+      if (verbose)
+        std::cout << " Result is written to `" << luminosity << "'\n";
+    };
+  };
+
+  epa::Function1d f_luminosity_b;
+  if (luminosity_b || survival) {
+    if (verbose)
+      std::cout
+        << "  with non-electromagnetic interactions between protons "
+           "respected:\n";
+    f_luminosity_b = pp_luminosity_b(
+        collision_energy, {1, 1}, grid, verbose ? "luminosity_b " : nullptr
+    );
+    if (verbose) std::cout << "Done.";
+    f_luminosity_b.save(luminosity_b);
+    if (verbose)
+      std::cout << " Result is written to `" << luminosity_b << "'\n";
+  };
+
+  if (survival) {
+    if (verbose) std::cout << "Calculating ratio... ";
+    epa::Function1d f_survival;
+    f_survival.points.resize(grid.size());
+    for (size_t i = 0; i < grid.size(); ++i) {
+      f_survival.points[i].first = grid[i].first;
+      f_survival.points[i].second = f_luminosity_b.points[i].second
+                                  / f_luminosity.points[i].second;
+    };
+    if (verbose) std::cout << "done.";
+    f_survival.save(survival);
+    if (verbose) std::cout << " Result is written to `" << survival << "'\n";
+  };
+
+  if (parallel) {
+    if (verbose) std::cout << "  for parallel polarization:\n";
+    pp_luminosity_b(
+        collision_energy, {1, 0}, grid, verbose ? "parallel " : nullptr
+    ).save(parallel);
+    if (verbose)
+      std::cout << "Done. Result is written to `" << parallel << "'\n";
+  };
+
+  if (perpendicular) {
+    if (verbose) std::cout << "  for perpendicular polarization:\n";
+    pp_luminosity_b(
+        collision_energy, {0, 1}, grid, verbose ? "perpendicular " : nullptr
+    ).save(perpendicular);
+    if (verbose)
+      std::cout << "Done. Result is written to `" << perpendicular << "'\n";
+  };
+
+  return 0;
+};
---
+++ examples/survival/survival.plot
@@ -0,0 +1,32 @@
+set term 'cairolatex' dashed lw 2 size 6.8 in, 3.9 in
+set output 'survival-plot.tex'
+
+set lmargin 8.1
+set rmargin 8
+
+set xtics add (1)
+set y2tics out
+set logscale y
+set format y '#@%.0E'
+set format y2 '%4.2f'
+
+set grid
+
+set xlabel '$\sqrt{s}$, GeV'
+set ylabel '$\mathrm{d} L / \mathrm{d} \sqrt{s},~\text{GeV}^{-1}$' \
+           offset character 3
+set y2label 'Survival factor' offset character -1
+
+set key Left reverse samplen 4
+
+plot [1 : 1e3] \
+     'luminosity.txt' w l lc rgb 'red' dt (20, 5) \
+     t '$\mathrm{d} L / \mathrm{d} \sqrt{s} \rvert_{P = 1}$', \
+     'luminosity_b.txt' w l lc rgb 'blue' dt (20, 5, 5, 5) \
+     t '$\mathrm{d} L / \mathrm{d} \sqrt{s}$', \
+     'parallel.txt' w l lc rgb 'dark-green' dt (20, 20) \
+     t '$\mathrm{d} L_\parallel / \mathrm{d} \sqrt{s}$', \
+     'perpendicular.txt' w l lc rgb 'brown' dt (5, 5) \
+     t '$\mathrm{d} L_\perp / \mathrm{d} \sqrt{s}$', \
+     'survival.txt' axes x1y2 w l lc rgb 'black' \
+     t 'Survival factor'
