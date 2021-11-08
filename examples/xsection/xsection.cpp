00a65d6ca532da1d8aac3d54a8515656c4e482ca
Eugene Zhemchugov <jini.zh@gmail.com>
2021-11-08 17:12:40 +0300

    Implement examples/xsection

---
+++ examples/xsection/xsection.cpp
@@ -0,0 +1,174 @@
+#include "../common.cpp"
+
+#include <getopt.h>
+
+void usage(const char* argv0) {
+  std::cout
+    << "This program plots the cross section for the production of a pair of"
+       "fermions in ultraperipheral proton-proton collisions with respect to"
+       "the fermion mass\n"
+       "Usage: " << argv0 << " options...\n"
+       "Allowed options:\n"
+       "  -h or --help:             print this message and exit (no argument)\n"
+       "  -E or --collision-energy: specify proton-proton collision energy (in GeV). Default is 13e3\n"
+       "  -f or --from:             beginning of the mass range (GeV)\n"
+       "  -t or --to:               ending of the mass range (GeV)\n"
+       "  -n or --npoints:          number of points in the mass range\n"
+       "  -s or --step:             points step in the mass range (GeV)\n"
+       "  -l or --log:              assume logarithmic scale in the mass range (no argument)\n"
+    << "  -v or --verbose:          be verbose while calculating\n"
+       "  -S or --survival:         account for non-electromagnetic interactions\n"
+       "  --rs-max:                 cut off integration over invariant mass (sqrt{s}) at this value (GeV)\n"
+  ;
+};
+
+int main(int argc, char** argv) {
+  gsl::init();
+  epa::default_relative_error = 1e-2;
+
+  option options[] = {
+    { "help",             0, nullptr, 'h' },
+    { "collision-energy", 1, nullptr, 'E' },
+    { "from",             1, nullptr, 'f' },
+    { "to",               1, nullptr, 't' },
+    { "npoints",          1, nullptr, 'n' },
+    { "step",             1, nullptr, 's' },
+    { "log",              0, nullptr, 'l' },
+    { "verbose",          0, nullptr, 'v' },
+    { "survival",         0, nullptr, 'S' },
+    { "rs-max",           1, nullptr,  -2 },
+    { nullptr,            0,       0,   0 }
+  };
+
+  std::string optstring;
+  {
+    std::stringstream ss;
+    for (auto& o: options)
+      if (o.val > 0) {
+        ss << static_cast<char>(o.val);
+        if (o.has_arg) ss << ':';
+      };
+    optstring = ss.str();
+  };
+
+  double collision_energy = 13e3;
+  double from = -1;
+  double to = -1;
+  double step = 0;
+  bool log = false;
+  unsigned npoints = 0;
+  bool verbose = false;
+  bool survival = false;
+  double rs_max = 0;
+
+  while (true) {
+    int c = getopt_long(argc, argv, optstring.c_str(), options, nullptr);
+    if (c == -1) break;
+    switch (c) {
+      case 'h':
+        usage(argv[0]);
+        return 0;
+
+      case 'E':
+        collision_energy = parse_energy(optarg, "collision energy");
+        break;
+
+      case 'f':
+        from = parse_energy(optarg, "beginning invariant mass (from)");
+        break;
+
+      case 't':
+        to = parse_energy(optarg, "ending invariant mass (to)");
+        break;
+
+      case 's':
+        step = parse_energy(optarg, "invariant mass step");
+        break;
+
+      case 'n':
+        npoints = parse_unsigned(optarg, "number of points");
+        break;
+
+      case 'l':
+        log = true;
+        break;
+
+      case 'v':
+        verbose = true;
+        break;
+
+      case 'S':
+        survival = true;
+        break;
+
+      case -2:
+        rs_max = parse_energy(optarg, "invariant mass cutoff");
+        break;
+
+      case '?':
+        return 1;
+    };
+  };
+
+  if ((from > 0) + (to > 0) + (step > 0) + (npoints > 0) != 3) {
+    std::cerr << "Exactly 3 of --from, --to, --step, --npoints must be specified\n";
+    return 1;
+  };
+
+  if (from < 0) {
+    from = to - step * npoints;
+    if (from < 0) {
+      std::cerr << "Beginning of the invariant mass range cannot be negative or zero\n";
+      return 1;
+    };
+  };
+
+  if (!npoints) npoints = (to - from) / step + 1;
+
+  if (rs_max == 0) rs_max = collision_energy / 2;
+
+  auto grid = make_grid(from, to, npoints, log);
+
+  auto xsection = survival
+                ? make_function1d_async(
+                    [=]() -> std::function<double (double)> {
+                      return [
+                        =,
+                        integrate  = epa::default_integrator(0),
+                        luminosity = epa::pp_luminosity_b(
+                            collision_energy,
+                            epa::default_integrator,
+                            1
+                        )
+                      ](double mass) -> double {
+                        return integrate(
+                            epa::xsection_b(epa::photons_to_fermions_b(mass), luminosity),
+                            2 * mass,
+                            rs_max
+                        );
+                      };
+                    },
+                    grid,
+                    verbose ? std::string("cross section ") : std::string()
+                  )
+                : make_function1d(
+                    [
+                      =,
+                      integrate = epa::default_integrator(0),
+                      luminosity = epa::pp_luminosity(
+                        collision_energy, epa::default_integrator(1)
+                      )
+                    ](double mass) -> double {
+                      return integrate(
+                          epa::xsection(epa::photons_to_fermions(mass), luminosity),
+                          2 * mass,
+                          rs_max
+                      );
+                    },
+                    grid
+                  );
+
+  xsection.dump(std::cout);
+
+  return 0;
+};
---
+++ examples/xsection/xsection.plot
@@ -0,0 +1,26 @@
+set term 'cairolatex' dashed lw 2 size 6.8 in, 3.7 in
+set output 'xsection-plot.tex'
+
+set rmargin 8
+
+set format y  '%3.1f'
+set format y2 '%4.2f'
+
+set xlabel '$m_\chi$, GeV'
+set ylabel '$\sigma(pp \to pp \chi^+ \chi^-)$, fb'
+set y2label 'Ratio' offset -1
+
+set xtics add (90, 250)
+set y2tics out
+
+set grid
+
+set key Left reverse
+
+plot [90:250] \
+     'xsection.txt' u 1:($2 * 1e15) w l lc rgb 'red' dt (20, 6) \
+     t 'Cross section with non-electromagnetic interactions neglected', \
+     'xsection_b.txt' u 1:($2 * 1e15) w l lc rgb 'blue' dt (20, 5, 5, 5) \
+     t 'Cross section with non-electromagnetic interactions respected', \
+     'ratio.txt' w l axes x1y2 lc rgb 'black' \
+     t 'Ratio'
